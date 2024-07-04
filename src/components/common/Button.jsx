527aea3a322b62ea6a7a22e9bf33bf9b49ec34c9
rwaeng <rwaeng0421@gmail.com>
2024-07-04 21:51:16 +0900

    💄 ui: 공통 컴포넌트 | 버튼 퍼블리싱 #7

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import '../../global.css';
+
+const Button = ({ btnInfo }) => {
+  const { text, color, width, onClick } = btnInfo;
+
+  // color: [배경 색, 글꼴 색]
+  const btnColor = {
+    orange: ['var(--orange-pri)', 'white'],
+    jade: ['var(--jade-pri)', 'white'],
+    gray: ['var(--gray-100)', 'var(--gray-400)'],
+  };
+
+  return (
+    <BtnContainer color={btnColor[color]} width={width} onClick={onClick}>
+      {text}
+    </BtnContainer>
+  );
+};
+
+export default Button;
+
+const BtnContainer = styled.button`
+  width: ${(props) => (props.width ? props.width : '335px')};
+  height: 56px;
+
+  border-radius: 40px;
+  background-color: ${(props) =>
+    props.color ? props.color[0] : 'var(--gray-100)'};
+
+  text-align: center;
+  color: ${(props) => (props.color ? props.color[1] : 'var(--gray-400)')};
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 140%;
+`;
