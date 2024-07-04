c827ae18a35b21256fecd968020d6853beced9c7
Hyerin Sung <hyerinx@naver.com>
2024-07-04 14:22:39 +0900

    Revert "💚chore: 폴더 구조 설정"
    This reverts commit 2f2b3dd6ddaa675b36997da4e37122f5c03bb829.

--- src/api/client.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const client = axios.create();
-client.defaults.baseURL = `${process.env.SERVER_URL}`;
-client.defaults.withCredentials = true;
-
-export default client;
