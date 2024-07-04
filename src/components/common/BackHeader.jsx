4e797c04d3104ed04f36bb0b67eab8c25baeb53f
rwaeng <rwaeng0421@gmail.com>
2024-07-04 22:22:51 +0900

    💄 ui: 공통 컴포넌트 | 뒤로가기 헤더 텍스트 추가

--- src/components/common/BackHeader.jsx
+++ src/components/common/BackHeader.jsx
@@ -3,13 +3,14 @@ import { ReactComponent as Back } from '../../assets/common/btn_back.svg';
 import { useNavigate } from 'react-router-dom';
 
 // 뒤로가기 버튼 헤더
-const BackHeader = ({ onClick }) => {
+const BackHeader = ({ text, onClick }) => {
   const navigate = useNavigate();
   const goBack = () => navigate(-1);
 
   return (
     <HeaderContainer>
-      <BackBtn onClick={onClick ? onClick : goBack}></BackBtn>
+      <BackBtn onClick={onClick ? onClick : goBack} />
+      <Label>{text}</Label>
     </HeaderContainer>
   );
 };
@@ -17,6 +18,8 @@ const BackHeader = ({ onClick }) => {
 export default BackHeader;
 
 const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
   position: sticky;
   top: 0;
   width: 375px;
@@ -32,4 +35,16 @@ const BackBtn = styled(Back)`
   left: 20px;
 
   cursor: pointer;
+`;
+
+const Label = styled.span`
+  position: absolute;
+  top: 43px;
+
+  height: 17px;
+
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 120%;
 `;
