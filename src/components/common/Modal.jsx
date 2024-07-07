1d38c5ce51bb6e23799a9411eeeaa7af7afd114b
Hyerin Sung <hyerinx@naver.com>
2024-07-08 00:33:42 +0900

    💄ui: overflow-y: scroll; 삭제

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -28,7 +28,6 @@ const Modal = (props) => {
     document.body.style.cssText = `
         position: fixed;
         top: -${window.scrollY}px;
-        overflow-y: scroll;
         width: 100%;`;
     return () => {
       const scrollY = document.body.style.top;
