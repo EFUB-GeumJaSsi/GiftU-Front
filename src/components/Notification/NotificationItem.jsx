580fdfe7cc86983cf731249945c311d66099dd96
sj0919 <161692586+sj0919@users.noreply.github.com>
2024-07-09 14:56:33 +0900

    💄ui: img태그 추가 styled-component 수정
    svg -> img 태그를 활용해 프로필 사진을 대체하였고 styled-components를 전반적으로 수정함

--- src/components/Notification/NotificationItem.jsx
+++ src/components/Notification/NotificationItem.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const NotificationItem = ({ name, message, time }) => {
+const NotificationItem = ({ image, name, message, time }) => {
   return (
     <ItemBox>
-      <svg
-        width='40'
-        height='40'
-        viewBox='0 0 40 40'
-        fill='none'
-        xmlns='http://www.w3.org/2000/svg'
-      >
-        <rect
-          x='0.5'
-          y='0.5'
-          width='39'
-          height='39'
-          rx='19.5'
-          fill='#D4D4D4'
-          stroke='#F3F4F5'
-        />
-      </svg>
+      <img src={image}></img>
       <TextBox>
         <Text>
           <Name>{name}</Name>
@@ -34,37 +18,39 @@ const NotificationItem = ({ name, message, time }) => {
 export default NotificationItem;
 
 const ItemBox = styled.div`
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: #d4d4d4;
+  }
   display: flex;
   flex-direction: row;
   align-items: center;
   width: 335px;
   height: 72px;
-  padding-left: 8px;
-  border: 0px 0px 1px 9px;
-  border-bottom: 1px solid #f3f4f5;
+  gap: 10px;
+  border-bottom: 1px solid var(--gray-100);
 `;
 const TextBox = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   gap: 5px;
-  padding-top: 2px;
 `;
 const Text = styled.div`
   display: flex;
   flex-direction: row;
   font-size: 16px;
-  padding-left: 10px;
   gap: 4px;
   width: 268px;
   height: 22px;
   overflow: hidden;
 `;
 const Time = styled.div`
-  padding-left: 10px;
   font-size: 12px;
   font-weight: 500;
-  color: #909294;
+  color: var(--gray-500);
 `;
 const Name = styled.div`
   font-weight: 500;
