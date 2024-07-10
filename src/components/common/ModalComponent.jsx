ab823e669faba08bb5081a7396d495543846d2a4
Hyerin Sung <hyerinx@naver.com>
2024-07-11 01:22:32 +0900

    ♻️refactor: 변경된 코딩 컨벤션 적용

--- src/components/common/ModalComponent.jsx
+++ src/components/common/ModalComponent.jsx
@@ -1,27 +1,32 @@
+import styled from 'styled-components';
 import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
-import styled from 'styled-components';
 
 // 모달 컴포넌트 사용법
 //
 // 1. 모달 활성화/비활성화 제어
 // (1-1) 부모 컴포넌트에 추가: const [modalShow, setModalShow] = useState(false);
-// (1-2) 부모 컴포넌트에 추가: {modalShow && (<Modal></Modal>)}
+// (1-2) 부모 컴포넌트에 추가: {modalShow && (<ModalComponent></ModalComponent>)}
 //
 // 2. 모달 props
 // (2-1) actionText - 오른쪽 버튼의 텍스트
 // (2-2) onClickAction - 오른쪽 버튼을 click할 경우 실행하는 function
 // (2-3) setModalShow - 1-1의 setModalShow
-// 예시: <Modal actionText='취소하기' onClickAction={api function} setModalShow={setModalShow}>
+// 예시: <ModalComponent actionText='취소하기' onClickAction={api function} setModalShow={setModalShow}>
 //
-// 3. 모달 Content
-// (3-1) 부모 컴포넌트에서 <Modal></Modal> 안에 작성합니다.
-// 예시: <Modal>
+// 3. 모달 내용
+// (3-1) 부모 컴포넌트에서 <ModalComponent></ModalComponent> 안에 작성합니다.
+// 예시: <ModalComponent>
 //         <span>펀딩 개설을 취소하시겠어요?</span>
 //         <small>펀딩에 참여한 친구들에게 알림이 전송돼요</small>
-//       </Modal>
+//       </ModalComponent>
 
-const Modal = (props) => {
+const ModalComponent = ({
+  actionText,
+  onClickAction,
+  setModalShow,
+  children,
+}) => {
   useEffect(() => {
     // 모달 활성화 시 스크롤 방지
     document.body.style.cssText = `
@@ -37,29 +42,31 @@ const Modal = (props) => {
     };
   }, []);
 
-  const ModalComponent = (
-    <Background>
-      <Container>
-        <Content>{props.children}</Content>
-        <Buttons>
-          <Cancel
+  const ModalUI = (
+    <SBackgroundDiv>
+      <SSection>
+        <SContentContainer>{children}</SContentContainer>
+        <SButtonContainer>
+          <SCancelButton
             onClick={() => {
-              props.setModalShow(false);
+              setModalShow(false);
             }}
           >
             돌아가기
-          </Cancel>
-          <Action onClick={props.onClickAction}>{props.actionText}</Action>
-        </Buttons>
-      </Container>
-    </Background>
+          </SCancelButton>
+          <SActionButton onClick={onClickAction}>{actionText}</SActionButton>
+        </SButtonContainer>
+      </SSection>
+    </SBackgroundDiv>
   );
 
-  return createPortal(ModalComponent, document.getElementById('modal'));
+  return createPortal(ModalUI, document.getElementById('modal'));
 };
 
-const Background = styled.div`
+const SBackgroundDiv = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: center;
   position: fixed;
   top: 0;
   left: 0;
@@ -69,43 +76,39 @@ const Background = styled.div`
   height: 100vh;
 
   background-color: #00000066;
-
-  align-items: center;
-  justify-content: center;
 `;
-const Container = styled.section`
+const SSection = styled.section`
   width: 320px;
 
   border-radius: 20px;
   background-color: var(--white);
 `;
-const Content = styled.article`
+const SContentContainer = styled.div`
   display: flex;
   flex-flow: column nowrap;
   align-items: center;
 
   padding: 30px 50px;
 `;
-const Buttons = styled.article`
+const SButtonContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
 
   height: 56px;
 
   border-top: 1px solid var(--gray-100);
-
-  button {
-    font-size: 16px;
-    font-weight: 500;
-  }
 `;
-const Cancel = styled.button`
+const SButton = styled.button`
+  font-size: 16px;
+  font-weight: 500;
+`;
+const SCancelButton = styled(SButton)`
   border-right: 1px solid var(--gray-100);
 
   color: var(--black);
 `;
-const Action = styled.button`
+const SActionButton = styled(SButton)`
   color: var(--red);
 `;
 
-export default Modal;
+export default ModalComponent;
