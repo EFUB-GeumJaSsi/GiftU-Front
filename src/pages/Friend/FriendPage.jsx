f630c9a3e8d7368e3d77e1a2646a0aa6359a496b
Hyerin Sung <hyerinx@naver.com>
2024-07-13 22:05:14 +0900

    💄ui: 친구 페이지 버튼 컴포넌트 퍼블리싱

--- src/pages/Friend/FriendPage.jsx
+++ src/pages/Friend/FriendPage.jsx
@@ -1,67 +1,71 @@
 import styled from 'styled-components';
 import { arrayChop } from '../../components/common/CarouselComponent';
 import CarouselComponent from '../../components/common/CarouselComponent';
-import Button from '../../components/common/Button';
 import VerticalCard from '../../components/Friend/VerticalCard';
 import HorizontalCard from '../../components/Friend/HorizontalCard';
+import { ReactComponent as IcnUnion } from '../../assets/Friend/icn_union.svg';
 
-const data = [
-  {
-    nickname: '닉네임1',
-    birthday: '3월 18일',
-  },
-  {
-    nickname: '닉네임2',
-    birthday: '5월 14일',
-  },
-  {
-    nickname: '닉네임999999999999999999999999',
-    birthday: '12월 21일',
-  },
-];
+const data = null;
+// const data = [
+//   {
+//     nickname: '닉네임1',
+//     birthday: '3월 18일',
+//   },
+//   {
+//     nickname: '닉네임2',
+//     birthday: '5월 14일',
+//   },
+//   {
+//     nickname: '닉네임999999999999999999999999',
+//     birthday: '12월 21일',
+//   },
+// ];
 
 const FriendPage = () => {
-  const chopedDataList = arrayChop(data, 2);
+  const chopedDataList = data && arrayChop(data, 2);
 
   return (
     <SLayout>
       <SHeader>친구</SHeader>
-      <SSection>
-        <ST1>나에게 선물한 친구</ST1>
-        <CarouselComponent pageLength={chopedDataList.length} pageWidth={335}>
-          {chopedDataList.map((item, index) => (
-            <SPageContainer key={index}>
-              <VerticalCard data={item[0]} />
-              {index === chopedDataList.length - 1 && data.length % 2 !== 0 ? (
-                <div style={{ visibility: 'hidden' }}>
-                  <VerticalCard />
-                </div>
-              ) : (
-                <VerticalCard data={item[1]} />
-              )}
-            </SPageContainer>
-          ))}
-        </CarouselComponent>
-      </SSection>
+      {data && (
+        <SSection>
+          <ST1>나에게 선물한 친구</ST1>
+          <CarouselComponent pageLength={chopedDataList.length} pageWidth={335}>
+            {chopedDataList.map((item, index) => (
+              <SPageContainer key={index}>
+                <VerticalCard data={item[0]} />
+                {index === chopedDataList.length - 1 &&
+                data.length % 2 !== 0 ? (
+                  <div style={{ visibility: 'hidden' }}>
+                    <VerticalCard />
+                  </div>
+                ) : (
+                  <VerticalCard data={item[1]} />
+                )}
+              </SPageContainer>
+            ))}
+          </CarouselComponent>
+        </SSection>
+      )}
       <SSection>
         <STopContainer>
           <ST1>내 친구</ST1>
-          <Button
-            btnInfo={{
-              text: '+ 친구 추가',
-              color: 'jade',
-              width: '84px',
-              onClick: '',
-            }}
-          />
+          <SBtn>
+            <IcnUnion />
+            친구 추가
+          </SBtn>
         </STopContainer>
-        <SUl>
-          {data.map((item, index) => (
-            <li key={index}>
-              <HorizontalCard data={item} />
-            </li>
-          ))}
-        </SUl>
+        {data ? (
+          <SUl>
+            {data.map((item, index) => (
+              <li key={index}>
+                <HorizontalCard data={item} />
+              </li>
+            ))}
+          </SUl>
+        ) : (
+          <SB1>친구에게 초대 메시지를 보내보세요!</SB1>
+        )}
       </SSection>
     </SLayout>
   );
@@ -72,7 +76,7 @@ const SLayout = styled.div`
   overflow-x: hidden;
   flex-flow: column nowrap;
 
-  width: fit-content;
+  width: 335px;
   margin: 40px auto 104px auto;
   gap: 24px;
 `;
@@ -106,11 +110,34 @@ const STopContainer = styled.div`
   justify-content: space-between;
   align-items: center;
 `;
+const SBtn = styled.button`
+  border-radius: 20px;
+  background-color: var(--jade-pri);
+
+  color: var(--white);
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 120%;
+
+  padding: 8px 12px 8px 8px;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  gap: 8px;
+`;
 const SUl = styled.ul`
   display: flex;
   flex-flow: column nowrap;
 
   gap: 16px;
+`;
+const SB1 = styled.p`
+  text-align: center;
+
+  color: var(--gray-500);
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 140%;
 `;
 
 export default FriendPage;
