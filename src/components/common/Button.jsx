0054b8ecc6fb8f9c6054603aeaf84e664325c9f4
rwaeng <rwaeng0421@gmail.com>
2024-07-13 17:05:09 +0900

    🎨 style: 공통 컴포넌트 주석 추가

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,13 +1,23 @@
 import styled from 'styled-components';
 import '../../global.css';
 
+// btnInfo라는 객체를 props로 받습니다.
+// btnInfo에는 text, color, width, onClick 값을 설정할 수 있습니다.
+//
+// text: 버튼에 적힐 내용,
+// color(생략 시 회색 버튼): 버튼 색상(글꼴 색상은 고려 안 하셔도 되며 orange, jade, gray 셋 중 하나로 작성하시면 됩니다),
+// width(생략 시 335px): 버튼 너비,
+// onClick: 이벤트 함수
+//
+// 예시 <Button btnInfo={{ text: '다음', color: 'jade' }} />
+
 const Button = ({ btnInfo }) => {
   const { text, color, width, onClick } = btnInfo;
 
   // color: [배경 색, 글꼴 색]
   const btnColor = {
-    orange: ['var(--orange-pri)', 'white'],
-    jade: ['var(--jade-pri)', 'white'],
+    orange: ['var(--orange-pri)', 'var(--white)'],
+    jade: ['var(--jade-pri)', 'var(--white)'],
     gray: ['var(--gray-100)', 'var(--gray-400)'],
   };
 
