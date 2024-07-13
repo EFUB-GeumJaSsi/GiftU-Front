5b17433be6b43336aeb5721758c8deead60c4102
sj0919 <161692586+sj0919@users.noreply.github.com>
2024-07-13 21:07:29 +0900

    💄ui: orange 색상 추가
    공용 컴포넌트에 orange 색상도 선택할 수 있도록 추가함

--- src/components/common/NotiItemComponent.jsx
+++ src/components/common/NotiItemComponent.jsx
@@ -1,31 +1,40 @@
+import React from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
-const NotiItem = ({
-  tagFirstName,
-  tagFirstLink,
-  tagSecondName,
-  tagSecondLink,
-}) => {
+
+// <NotiItem
+//  buttons={[
+//  { text: '전체', link: '/', activeColor: 'jade' },
+//  { text: '친구', link: '/first', activeColor: 'jade' },
+//  { text: '펀딩', link: '/second', activeColor: 'jade' },
+// ]}
+// />
+// 위의 코드는 예시입니다. text에 원하는 문구를 적고 ,link는 그대로 사용하고, activeColor에는 jade와 orange중에 선택해서 사용하면 됩니다.
+const NotiItem = ({ buttons }) => {
+  // activeColor: [배경 색, 글꼴 색]
+  const btnActiveColor = {
+    orange: ['var(--orange-pri)', 'var(--white)'],
+    jade: ['var(--jade-pri)', 'var(--white)'],
+    gray: ['var(--gray-100)', 'var(--gray-400)'],
+  };
+
   return (
     <SLayout>
-      <SBtn>
-        <StyledNavLink to='/' activeclassname='active'>
-          전체
-        </StyledNavLink>
-      </SBtn>
-      <SBtn>
-        <StyledNavLink to={tagFirstLink} activeclassname='active'>
-          {tagFirstName}
-        </StyledNavLink>
-      </SBtn>
-      <SBtn>
-        <StyledNavLink to={tagSecondLink} activeclassname='active'>
-          {tagSecondName}
-        </StyledNavLink>
-      </SBtn>
+      {buttons.map((btn, index) => (
+        <SBtn key={index}>
+          <StyledNavLink
+            to={btn.link}
+            activeclassname='active'
+            activeColor={btnActiveColor[btn.activeColor]}
+          >
+            {btn.text}
+          </StyledNavLink>
+        </SBtn>
+      ))}
     </SLayout>
   );
 };
+
 export default NotiItem;
 
 const SLayout = styled.div`
@@ -34,7 +43,7 @@ const SLayout = styled.div`
   gap: 8px;
 `;
 
-const SBtn = styled.button`
+const SBtn = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -51,11 +60,12 @@ const StyledNavLink = styled(NavLink)`
   align-items: center;
   width: 100%;
   height: 100%;
-  color: white;
+  color: inherit;
   text-decoration: none;
 
   &.active {
-    background-color: var(--jade-pri);
+    background-color: ${(props) => props.activeColor[0]};
+    color: ${(props) => props.activeColor[1]};
     border-radius: 15px;
   }
 `;
