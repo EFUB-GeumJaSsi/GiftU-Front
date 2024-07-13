324847bc14215a5b2b47ea707a6dee670237f93b
Hyerin Sung <hyerinx@naver.com>
2024-07-14 06:04:09 +0900

    ♻️refactor: 바텀시트 컴포넌트

--- src/components/common/BottomsheetComponent.jsx
+++ src/components/common/BottomsheetComponent.jsx
@@ -1,101 +1,68 @@
 // 바텀시트 컴포넌트 사용법
 //
 // 1. 바텀시트 열기/닫기 제어
-// (1-1) 부모 컴포넌트에 추가: const [bottomsheetShow, setBottomsheetShow] = useState(false);
-// (1-2) 부모 컴포넌트에 추가: {bottomsheetShow && (<BottomsheetComponent></BottomsheetComponent>)}
+// (1-1) 부모 컴포넌트에 추가: const [bottomSheetShow, setBottomSheetShow] = useState(false);
+// (1-2) 부모 컴포넌트에 추가: {bottomSheetShow && (<BottomSheetComponent></BottomSheetComponent>)}
 //
 // 2. 바텀시트 props
 // (2-1) closeButton - 'bar' 또는 'cross' (기본 'bar')
-// (2-2) setBottomsheetShow - 1-1의 setBottomsheetShow
-// 예시: <BottomsheetComponent closeButton='cross' setBottomsheetShow={setBottomsheetShow}>
+// (2-2) setBottomSheetShow - 1-1의 setBottomSheetShow
+// 예시: <BottomSheetComponent closeButton='cross' setBottomSheetShow={setBottomSheetShow}>
 //
 // 3. 바텀시트 내용
-// (3-1) 부모 컴포넌트에서 <BottomsheetComponent></BottomsheetComponent> 안에 작성합니다.
-// 예시: <BottomsheetComponent>
+// (3-1) 부모 컴포넌트에서 <BottomSheetComponent></BottomSheetComponent> 안에 작성합니다.
+// 예시: <BottomSheetComponent>
 //         <h1>친구 추가</h1>
 //         <input type='email' name='user-email' placeholder='이메일 입력' />
-//       </BottomsheetComponent>
+//       </BottomSheetComponent>
 
 import styled from 'styled-components';
-import { useEffect, useState } from 'react';
-import { createPortal } from 'react-dom';
+import { useState, useEffect } from 'react';
+import BottomModalComponent from './BottomModalComponent';
 import { ReactComponent as CrossIcon } from '../../assets/common/Bottomsheet/cross.svg';
 
-const BottomsheetComponent = ({
+const BottomSheetComponent = ({
   closeButton,
-  setBottomsheetShow,
+  setBottomSheetShow,
   children,
 }) => {
-  const [open, setOpen] = useState(false);
+  const [bottomSheetOpen, setBottomSheetOpen] = useState(false);
 
-  const handleBottomsheetClose = () => {
-    setOpen(false); // 바텀시트 닫기 애니메이션 효과
-    setTimeout(() => setBottomsheetShow(false), 300); // 애니메이션 후 언마운트
+  const handleBottomSheetClose = () => {
+    setBottomSheetOpen(false); // 바텀시트 닫기 애니메이션 효과
+    setTimeout(() => setBottomSheetShow(false), 300); // 애니메이션 후 언마운트
   };
 
   useEffect(() => {
     // 바텀시트 열기 애니메이션 효과
-    setOpen(true);
-    // 바텀시트 열기 시 스크롤 방지
-    document.body.style.cssText = `
-      position: fixed;
-      top: -${window.scrollY}px;
-      width: 100%;
-    `;
-    return () => {
-      // 바텀시트 닫기 시 이전 상태로 복원
-      const scrollY = document.body.style.top;
-      document.body.style.cssText = '';
-      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
-    };
+    setBottomSheetOpen(true);
   }, []);
 
-  const BottomsheetUI = (
-    <SBackgroundDiv onClick={handleBottomsheetClose} $open={open}>
+  return (
+    <BottomModalComponent
+      setBottomModalShow={setBottomSheetShow}
+      parentOpen={bottomSheetOpen}
+    >
       <SSection
         onClick={(event) => {
           event.stopPropagation();
         }}
-        $open={open}
         $cross={closeButton === 'cross'}
       >
         {closeButton === 'cross' ? (
-          <SCrossButton onClick={handleBottomsheetClose}>
+          <SCrossButton onClick={handleBottomSheetClose}>
             <CrossIcon />
           </SCrossButton>
         ) : (
-          <SBarButton onClick={handleBottomsheetClose} />
+          <SBarButton onClick={handleBottomSheetClose} />
         )}
         <article>{children}</article>
       </SSection>
-    </SBackgroundDiv>
+    </BottomModalComponent>
   );
-
-  return createPortal(BottomsheetUI, document.getElementById('modal'));
 };
 
-const SBackgroundDiv = styled.div`
-  display: flex;
-  justify-content: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 9999;
-
-  width: 100vw;
-  height: 100vh;
-
-  background-color: ${({ $open }) =>
-    $open ? 'rgba(0, 0, 0, 0.4)' : 'rgba(0, 0, 0, 0)'};
-
-  transition: background-color 0.3s ease;
-`;
 const SSection = styled.section`
-  display: flex;
-  flex-flow: column nowrap;
-  position: fixed;
-  bottom: ${({ $open }) => ($open ? 0 : -320)}px;
-
   width: 375px;
   height: 320px;
   padding-top: ${({ $cross }) => ($cross ? '26px' : '16px')};
@@ -103,8 +70,6 @@ const SSection = styled.section`
 
   border-radius: 20px 20px 0px 0px;
   background-color: var(--white);
-
-  transition: bottom 0.3s ease;
 
   box-sizing: border-box;
 `;
@@ -121,4 +86,4 @@ const SBarButton = styled.button`
   border-radius: 12px;
 `;
 
-export default BottomsheetComponent;
+export default BottomSheetComponent;
