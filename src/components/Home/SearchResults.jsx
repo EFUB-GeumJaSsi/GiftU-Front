95c9388a8a16ed096466227cf7ebe411908977c8
sj0919 <161692586+sj0919@users.noreply.github.com>
2024-07-15 19:53:27 +0900

    🔧fix: 코드오류 수정

--- src/components/Home/SearchResults.jsx
+++ src/components/Home/SearchResults.jsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 const SearchResults = ({ results }) => {
   return (
     <SResultsContainer>
@@ -31,3 +32,86 @@ const SearchResults = ({ results }) => {
 };
 
 export default SearchResults;
+
+const SResultsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+`;
+const SNoresultsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 500px;
+  color: var(--gray-500);
+`;
+const SResultItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  height: 136px;
+  width: 335px;
+  img {
+    width: 120px;
+    height: 120px;
+    background-color: var(--gray-300);
+    border-radius: 16px;
+  }
+`;
+
+const SImageWrapper = styled.div``;
+const SContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 8px;
+  font-weight: 500;
+  font-size: 14px;
+  #title {
+    font-weight: 700;
+    font-size: 17px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: 100%;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
+  #name {
+    display: flex;
+    flex-direction: row;
+    gap: 4px;
+  }
+  #endDate {
+    display: flex;
+    flex-direction: row;
+    gap: 4px;
+  }
+  #tag {
+    display: flex;
+    flex-direction: row;
+    gap: 7px;
+    margin-top: 3px;
+    #stateTag {
+      background-color: var(--jade-sec);
+      color: var(--jade-pri);
+      border-radius: 20px;
+      padding: 4px 8px 4px 8px;
+    }
+    #isFriend {
+      background-color: var(--orange-sec);
+      color: var(--orange-pri);
+      border-radius: 20px;
+      padding: 4px 8px 4px 8px;
+    }
+  }
+`;
+const SBoldWrapper = styled.div`
+  color: var(--gray-500);
+  font-weight: 500;
+`;
