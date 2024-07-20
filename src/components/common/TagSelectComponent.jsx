537856aa6b02b97fcb2cb5675846f73aa0608aa0
sj0919 <161692586+sj0919@users.noreply.github.com>
2024-07-20 14:58:11 +0900

    💄ui: 2주차 수정사항 반영
    아이템들이 카테고리별로 나눠서 보이도록 수정

--- src/components/common/TagSelectComponent.jsx
+++ src/components/common/TagSelectComponent.jsx
@@ -1,35 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
+//const handleCategoryChange = (category) => {
+//  setSelectedCategory(category);
+//};
 // <TagSelectComponent
 //  buttons={[
-//  { text: '전체', link: '/', color: 'jade' },
-//  { text: '친구', link: '/first', color: 'jade' },
-//  { text: '펀딩', link: '/second', color: 'jade' },
+//  { text: '전체', color: 'jade' },
+//  { text: '친구', color: 'jade' },
+//  { text: '펀딩', color: 'jade' },
 //  ]}
+// onClick={handleCategoryChange}
 // />
-// 위의 코드는 예시입니다. text에 원하는 문구를 적고, link는 그대로 사용하고, color는 jade와 orange 중에 선택하면 됩니다.
+// text에 원하는 문구를 적고, color는 jade와 orange 중에 선택하면 됩니다.
 
-const TagSelectComponent = ({ buttons }) => {
+const TagSelectComponent = ({ buttons, onClick }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const click = (index, text) => {
+    setActiveIndex(index);
+    onClick(text);
+  };
   // color: [배경 색, 글꼴 색]
   const btnColor = {
-    orange: ['var(--orange-pri)', 'var(--white)'],
-    jade: ['var(--jade-pri)', 'var(--white)'],
-    gray: ['var(--gray-100)', 'var(--gray-400)'],
+    orange: ['var(--orange-pri)'],
+    jade: ['var(--jade-pri)'],
+    gray: ['var(--gray-100)'],
   };
 
   return (
     <SLayout>
       {buttons.map((btn, index) => (
-        <SBtn key={index}>
-          <StyledNavLink
-            to={btn.link}
-            activeclassname='active'
-            color={btnColor[btn.color]}
-          >
-            {btn.text}
-          </StyledNavLink>
+        <SBtn
+          key={index}
+          isActive={index === activeIndex}
+          color={btnColor[btn.color]}
+          onClick={() => click(index, btn.text)}
+        >
+          {btn.text}
         </SBtn>
       ))}
     </SLayout>
@@ -50,21 +58,11 @@ const SBtn = styled.div`
   width: 57px;
   height: 29px;
   border-radius: 15px;
-  background-color: var(--gray-300);
-  color: white;
-`;
-const StyledNavLink = styled(NavLink)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  color: inherit;
-  text-decoration: none;
-
-  &.active {
-    background-color: ${(props) => props.color[0]};
-    color: ${(props) => props.color[1]};
-    border-radius: 15px;
-  }
+  cursor: pointer;
+  text-align: center;
+  line-height: 29px;
+  user-select: none;
+  background-color: ${(props) =>
+    props.isActive ? props.color[0] : 'var(--gray-300)'};
+  color:var(--white)};
 `;
