fba57b6051d76df291642332b9b4b9a850505889
ch9968 <ichanhui578@gmail.com>
2024-07-20 17:52:03 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-
+import PasswordSetPage from './pages/FundingOpen/PasswordSetPage';
+import FundingSet from './pages/FundingOpen/FundingSetPage';
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <PasswordSetPage />
+    </div>
+  );
 };
 
 export default App;
