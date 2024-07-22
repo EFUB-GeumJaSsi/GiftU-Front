f7597c67a674689dbeb2e9b1b4d73ae616246207
Chanhee Lee <127084774+ch9968@users.noreply.github.com>
2024-07-22 12:33:42 +0900

    Update src/App.js
    Co-authored-by: Aryeong Jo <80890092+rwaeng@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 const App = () => {
-  return (
-    <div className='App'>
-      <ListJoinPage />
-    </div>
-  );
+ return <div className='App'></div>;
 };
 
 export default App;
