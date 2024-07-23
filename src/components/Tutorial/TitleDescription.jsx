c2bc538614b44835636379f1eb218f53e05a121c
sj0919 <161692586+sj0919@users.noreply.github.com>
2024-07-23 16:01:10 +0900

    ♻️refactor: 코드 리팩토링
    3주차 리뷰 반영하여 코드 리팩토링

--- src/components/Tutorial/TitleDescription.jsx
+++ src/components/Tutorial/TitleDescription.jsx
@@ -8,29 +8,27 @@ const TitleDescription = ({
   component: Component,
 }) => {
   return (
-    <>
-      <SContentboxContainer>
-        <STitleBoxContainer>
-          <SNumWrapper color={color}>{num}</SNumWrapper>
-          <STitleWrapper>{title}</STitleWrapper>
-        </STitleBoxContainer>
-        <SExplainBoxWrapper>
-          <SExplainWrapper>{description}</SExplainWrapper>
-        </SExplainBoxWrapper>
-        {Component && <Component />}
-      </SContentboxContainer>
-    </>
+    <SLayout>
+      <STitleContainer>
+        <SNumWrapper color={color}>{num}</SNumWrapper>
+        <STitleWrapper>{title}</STitleWrapper>
+      </STitleContainer>
+      <STextWrapper>{description}</STextWrapper>
+      {Component && <Component />}
+    </SLayout>
   );
 };
 
-const SContentboxContainer = styled.div`
+export default TitleDescription;
+const SLayout = styled.div`
   display: flex;
   flex-direction: column;
   gap: 7px;
 
   width: 344px;
 `;
-const STitleBoxContainer = styled.div`
+
+const STitleContainer = styled.div`
   display: flex;
   flex-direction: row;
   gap: 8px;
@@ -48,21 +46,16 @@ const STitleWrapper = styled.div`
   font-weight: 600;
   font-size: 16px;
 `;
-const SExplainBoxWrapper = styled.div`
+const STextWrapper = styled.div`
   display: flex;
   padding: 0px 36px 0px 36px;
   gap: 8px;
 
   width: 307px;
-
   line-height: 19px;
-`;
-const SExplainWrapper = styled.div`
   margin-bottom: 5px;
 
   font-weight: 500;
   font-size: 14px;
   color: var(--gray-500);
 `;
-
-export default TitleDescription;
