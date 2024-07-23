25404af424f2e2cb953eb2df5f0387773804b758
Hyerin Sung <hyerinx@naver.com>
2024-07-24 02:58:46 +0900

    ✨feat: 친구 api

--- src/api/friend.js
+++ src/api/friend.js
@@ -1,13 +1,68 @@
 import { api, apiAuth } from './api';
 
 // 친구 요청
+export const postFriendRequest = async (email) => {
+  try {
+    const response = await apiAuth.post(
+      `/friends/request`,
+      {
+        email: email,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+    return Promise.resolve(response);
+  } catch (error) {
+    return Promise.resolve(error);
+  }
+};
 
 // 친구 요청 수락
+export const postFriendAccept = async (friendTableId) => {
+  try {
+    const response = await apiAuth.post(
+      `/friends/accept?friendTableId=${friendTableId}`,
+    );
+    return Promise.resolve(response);
+  } catch (error) {
+    return Promise.resolve(error);
+  }
+};
 
 // 친구 요청 거절
+export const postFriendReject = async (friendTableId) => {
+  try {
+    const response = await apiAuth.post(
+      `/friends/reject?friendTableId=${friendTableId}`,
+    );
+    return Promise.resolve(response);
+  } catch (error) {
+    return Promise.resolve(error);
+  }
+};
 
 // 친구 목록 조회
+export const getFriendList = async () => {
+  try {
+    const response = await apiAuth.get(`/friends`);
+    return Promise.resolve(response);
+  } catch (error) {
+    return Promise.resolve(error);
+  }
+};
 
 // 최근 내 펀딩에 참여한 친구 목록 조회
+// 백엔드 개발 중
 
 // 친구 삭제
+export const deleteFriend = async (friendId) => {
+  try {
+    const response = await apiAuth.delete(`/friends/${friendId}`);
+    return Promise.resolve(response);
+  } catch (error) {
+    return Promise.resolve(error);
+  }
+};
