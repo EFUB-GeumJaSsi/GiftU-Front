e2d2d2d7e33895a41ec91b9211eae80d320c8593
Hyerin Sung <hyerinx@naver.com>
2024-07-25 03:07:52 +0900

    ✨feat: 카카오 로그인/회원가입 API 연결 - (2) 기프트유 서버에 토큰 요청

--- src/api/oauth.js
+++ src/api/oauth.js
@@ -7,5 +7,17 @@ const FRONT_DOMAIN = `http://localhost:3000`;
 const REDIRECT_URI = `${FRONT_DOMAIN}/api/oauth/kakao`;
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 // 2. 기프트유 서버에 토큰 요청
+export const getKakaoAccessToken = async (code) => {
+  try {
+    const response = await api.get(`/api/oauth/kakao`, {
+      params: {
+        code: code,
+      },
+    });
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
 
 // 액세스 토큰 재발급
--- src/pages/Login/OAuthRedirectPage.jsx
+++ src/pages/Login/OAuthRedirectPage.jsx
@@ -1,7 +1,28 @@
 import styled from 'styled-components';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getKakaoAccessToken } from '../../api/oauth';
 import SpinnerComponent from '../../components/common/SpinnerComponent';
 
 const OAuthRedirectPage = () => {
+  const navigate = useNavigate();
+  const code = new URL(window.location.href).searchParams.get('code');
+
+  const fetchToken = async () => {
+    try {
+      const response = await getKakaoAccessToken(code);
+      localStorage.setItem('giftu-token', response.data.accessToken);
+      navigate('/my/edit');
+    } catch (error) {
+      console.error(error);
+      navigate('/login');
+    }
+  };
+
+  useEffect(() => {
+    fetchToken();
+  }, []);
+
   return (
     <SLayout>
       <SpinnerComponent />
