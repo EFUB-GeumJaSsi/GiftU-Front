b793343850c72f993e1fe260dccef716b2d85305
sj0919 <seungjinlee09@gmail.com>
2024-07-29 13:30:47 +0900

    ✨ feat: notice, review, search api 연결

--- src/api/notice.js
+++ src/api/notice.js
@@ -1,7 +1,38 @@
-import { api, apiAuth } from './api';
+import { apiAuth } from './api';
 
 // 모든 알림 조회
+export const getAllNotice = async () => {
+  try {
+    const response = await apiAuth.get(`/notice`, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+console.log(response);
 
 // 친구 알림 조회
+export const getFriendNotice = async () => {
+  try {
+    const response = await apiAuth.get(`/notice/friends`, {
+      'Content-Type': 'application/json',
+    });
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
 
 // 펀딩 알림 조회
+export const getFundingNotice = async () => {
+  try {
+    const response = await apiAuth.get(`/notice/funding`, {
+      'Content-Type': 'application/json',
+    });
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
