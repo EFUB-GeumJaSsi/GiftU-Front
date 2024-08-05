c816dccff9edfffd03bb56ae909431710b76b137
ch9968 <ichanhui578@gmail.com>
2024-08-05 21:07:49 +0900

    ♻️refactor: 수정사항 반영 List빼기

---
+++ src/components/List/List.jsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+import TagSelectComponent from '../common/TagSelectComponent';
+import FundingComponent from '../common/FundingComponent';
+
+const List = ({ tags, selectedTag, onTagChange, openList, color }) => {
+  return (
+    <SLayout>
+      <SNotiItemWrapper>
+        <TagSelectComponent
+          tags={tags}
+          selectedTag={selectedTag}
+          onTagChange={onTagChange}
+          color={color}
+        />
+      </SNotiItemWrapper>
+      {openList.length === 0 ? (
+        selectedTag === '전체' ? (
+          <SNoResultsWrapper>
+            {color === 'orange'
+              ? '아직 내가 참여한 펀딩이 없어요'
+              : '아직 내가 만든 펀딩이 없어요'}
+          </SNoResultsWrapper>
+        ) : selectedTag === '진행' ? (
+          <SNoResultsWrapper>진행 중인 펀딩이 없어요</SNoResultsWrapper>
+        ) : (
+          <SNoResultsWrapper>종료된 펀딩이 없어요</SNoResultsWrapper>
+        )
+      ) : (
+        <SFundingContainer>
+          {openList.map((funding, index) => (
+            <FundingComponent data={funding} key={index} />
+          ))}
+        </SFundingContainer>
+      )}
+    </SLayout>
+  );
+};
+
+const SLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  height: calc(100% - 72px);
+  width: 375px;
+`;
+const SNotiItemWrapper = styled.div`
+  margin-top: 24px;
+  margin-bottom: 20px;
+`;
+const SFundingContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 7px;
+  row-gap: 8px;
+
+  cursor: pointer;
+`;
+const SNoResultsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+
+  width: 100%;
+
+  background-color: var(--gray-100);
+  color: var(--gray-500);
+
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 140%;
+`;
+export default List;
