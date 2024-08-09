b62981f679bdca373914f24a6f89dc8af1a95e6a
sj0919 <seungjinlee09@gmail.com>
2024-08-09 13:56:28 +0900

    ♻️ refactor: 검색페이지 padding 추가

--- src/pages/Search/SearchPage.jsx
+++ src/pages/Search/SearchPage.jsx
@@ -134,53 +134,8 @@ const SOl = styled.ol`
 
   width: fit-content;
   margin: 0 auto;
+  padding-bottom: 40px;
   gap: 20px;
-`;
-const SResultItem = styled.button`
-  display: flex;
-  flex-direction: row;
-
-  align-items: center;
-
-  width: 332px;
-  gap: 15px;
-`;
-const SImg = styled.img`
-  width: 120px;
-  height: 120px;
-
-  border-radius: 16px;
-  background-color: var(--gray-300);
-`;
-const SContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-
-  width: 187px;
-
-  ${B3}
-`;
-const STitleText = styled.text`
-  display: -webkit-box;
-  text-align: start;
-
-  max-width: 100%;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-
-  ${T1}
-  overflow: hidden;
-  text-overflow: ellipsis;
-`;
-const SNameText = styled.text`
-  display: flex;
-  flex-direction: row;
-  gap: 4px;
-`;
-const SDateText = styled(SNameText)``;
-const SBoldWrapper = styled.div`
-  color: var(--gray-500);
 `;
 
 export default SearchPage;
