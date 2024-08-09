8027fd0a3945e1a20b8e6f65195f9f39a9a1ad15
Hyerin Sung <hyerinx@naver.com>
2024-08-09 18:18:35 +0900

    ♻️refactor: 카카오 로그인/회원가입, 액세스 토큰 재발급 기능 관련 코드 리팩토링

--- src/api/api.js
+++ src/api/api.js
@@ -6,10 +6,11 @@ const api = axios.create({
 });
 
 // 인증이 필요한 요청
-const accessToken = localStorage.getItem('token');
+const token = JSON.parse(localStorage.getItem('token'));
+const auth = token ? `${token.grantType} ${token.accessToken}` : null;
 const apiAuth = axios.create({
   baseURL: `${process.env.REACT_APP_SERVER_URL}`,
-  headers: { Authorization: `Bearer ${accessToken}` },
+  headers: { Authorization: auth },
   withCredentials: true,
 });
 
