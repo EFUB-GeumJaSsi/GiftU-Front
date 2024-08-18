06464225f118a2389eb28ddc6c9dcedf249136ff
Hyerin Sung <hyerinx@naver.com>
2024-08-19 02:28:10 +0900

    🎨style: 에러 메시지 수정

--- src/pages/FundingOpen/IndexPage.jsx
+++ src/pages/FundingOpen/IndexPage.jsx
@@ -56,7 +56,7 @@ const PageRenderer = () => {
       const response = await postFunding(request, imageData);
       setCurrentPage('CompletePage');
     } catch (error) {
-      console.error('펀딩 생성 오류', error);
+      console.error('펀딩 개설 오류', error);
       setToastShow(true);
       setCurrentPage('HomePage');
       window.history.replaceState(null, null, '/home');
